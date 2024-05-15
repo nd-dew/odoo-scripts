a638199440c627d24ea31b6241be637738f4c597
Andrzej(pian) <pian@odoo.com>
2024-05-15 09:27:23 +0200

    [ref] mailhogscript more consistent now

--- set_mailhog_for_db.py
+++ set_mailhog_for_db.py
@@ -1,33 +1,39 @@
 #!/usr/bin/env python3
 
 """
-This tool is sending mail to an Odoo instance (local or not).
+This tool is setting odoo instance to use Mailhog as an outgoing STMP mail server
 
-Usage:
-    send-mail.py <eml_file>
-					<-d <database>>
-					[-u <url> default:"http://localhost:8569"]
-					[-U <user> default:"admin"]
-					[-p <password> default:"admin"]
+Minimal Usage:
+    python set_mailhog_for_db -d <db_name>
+    python set_mailhog_for_db -d 17d
+The above: sets odoo running on url "http://o17d:8069" and with db called "17d", to use Mailhog as an outgoing STMP mail server
+
+            -d <database>>
+            [-u <url> default:"http://localhost:8569"]
+            [-U <user> default:"admin"]
+            [-p <password> default:"admin"]
 ]
 
-Commit odoo#ab70afb is removing the previously used route '/mail/receive'
-Now we use the xmlrpc function to log to the db and the odoo's method message_process of mail.thread,
-the first step on received emails.
 
-Working version:
-    8+
 """
+
 import base64
 import xmlrpc.client
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 
-parser = ArgumentParser(description="Send an email to Odoo databases", formatter_class=ArgumentDefaultsHelpFormatter)
-parser.add_argument("-u", "--url", dest="url", default="http://localhost:8569", help="URL of the Odoo server")
+parser = ArgumentParser(
+    description="Set Odoo instance to use Mailhog as an outgoing STMP mail server",
+    formatter_class=ArgumentDefaultsHelpFormatter,
+)
 parser.add_argument("-d", "--db", dest="db", required=True, help="Odoo database to orient to mailhog")
+parser.add_argument("-u", "--url", dest="url", default=None, help="URL of the Odoo server")
 parser.add_argument("-U", "--user", dest="username", default="admin", help="Login to connect with")
 parser.add_argument("-p", "--password", dest="password", default="admin", help="Password to connect with")
 args = parser.parse_args()
+
+if not args.url:
+    args.url = f"http://o{args.db}:8069"
+    print(f'Infered url:"{args.url}"')
 
 common = xmlrpc.client.ServerProxy("{}/xmlrpc/2/common".format(args.url))
 uid = common.authenticate(args.db, args.username, args.password, {})
@@ -38,6 +44,15 @@ models.execute_kw(
     args.password,
     "ir.mail_server",
     "create",
-    [{'name': 'Mailhog', 'from_filter': False, 'smtp_host': '0.0.0.0', 'smtp_port': 1025, 'smtp_debug': False,  'smtp_user': False, 'smtp_pass': False, 'smtp_ssl_certificate': False, 'smtp_ssl_private_key': False}],
+    [{
+        "name": "Mailhog",
+        "from_filter": False,
+        "smtp_host": "0.0.0.0",
+        "smtp_port": 1025,
+        "smtp_debug": False,
+        "smtp_user": False,
+        "smtp_pass": False,
+        "smtp_ssl_certificate": False,
+        "smtp_ssl_private_key": False,
+    }],
 )
-
