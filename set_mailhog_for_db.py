84ef2867d424e243553f7f3366f856a1e00ea663
Andrzej(pian) <pian@odoo.com>
2024-05-15 09:42:49 +0200

    [add] mailhogscript detects if Mailhog is running, informs user about it

--- set_mailhog_for_db.py
+++ set_mailhog_for_db.py
@@ -20,6 +20,18 @@ The above: sets odoo running on url "http://o17d:8069" and with db called "17d",
 import base64
 import xmlrpc.client
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+import requests, subprocess
+
+def is_mailhog_running(mailhog_ip):
+    url = f"http://{mailhog_ip}:8025/api/v2/messages"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except requests.ConnectionError:
+        return False
 
 parser = ArgumentParser(
     description="Set Odoo instance to use Mailhog as an outgoing STMP mail server",
@@ -29,7 +41,10 @@ parser.add_argument("-d", "--db", dest="db", required=True, help="Odoo database 
 parser.add_argument("-u", "--url", dest="url", default=None, help="URL of the Odoo server")
 parser.add_argument("-U", "--user", dest="username", default="admin", help="Login to connect with")
 parser.add_argument("-p", "--password", dest="password", default="admin", help="Password to connect with")
+parser.add_argument("-m", "--mailhog-url", dest="mailhog_url", default="0.0.0.0:1025", help="Mailhog URL under which STMP works (not HTTP)")
 args = parser.parse_args()
+
+mailhog_ip, mailhog_port = args.mailhog_url.split(":")
 
 if not args.url:
     args.url = f"http://o{args.db}:8069"
@@ -47,8 +62,8 @@ models.execute_kw(
     [{
         "name": "Mailhog",
         "from_filter": False,
-        "smtp_host": "0.0.0.0",
-        "smtp_port": 1025,
+        "smtp_host": mailhog_ip,
+        "smtp_port": mailhog_port,
         "smtp_debug": False,
         "smtp_user": False,
         "smtp_pass": False,
@@ -56,3 +71,7 @@ models.execute_kw(
         "smtp_ssl_private_key": False,
     }],
 )
+
+# Odoo set to use Mailhog, is Mailhog running ?
+if not is_mailhog_running(mailhog_ip):
+    print("Odoo set to use Mailhog, but Mailhog is NOT running")
