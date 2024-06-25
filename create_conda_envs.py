090782048013c8c2eb32a2714750a2d57daa20f1
Andrzej(pian) <pian@odoo.com>
2024-06-25 17:14:07 +0200

    improved msg in conda env creating
    now we have final msg post all work

--- create_conda_envs.py
+++ create_conda_envs.py
@@ -16,13 +16,17 @@ env_names = [
 
 
 # Iterate through the lists and create the environments
+msg="create_conda_envs.py"
 for env_name in env_names:
     # Check if the environment already exists
     output = subprocess.check_output("conda env list", text=True, shell=True)
     if any(env_name in line for line in output.splitlines()):
         print(f"Environment {env_name} already exists. Skipping...")
-    
+        msg+= f"\n\tEnvironment {env_name} already exists. Skipping..."
+
     else: # Environment does not exist; create it
         print(f"Creating environment {env_name} with Python {python_version}...")
         subprocess.run(f"conda create -n {env_name} python={python_version} -y", shell=True)
         subprocess.run(f"conda create -n {env_name} python={python_version} -y", shell=True)
+        msg+= f"\n\tEnvironment {env_name} Created"
+print(msg)
