2ea9e8f7bbfa0d150e37e173bf973fdc9dd9f656
Andrzej(pian) <pian@odoo.com>
2025-02-24 15:50:19 +0100

    ip fixed

--- versions.py
+++ versions.py
@@ -19,15 +19,15 @@ class Version():
 
 # Formal is the official branch name
 VERSIONS= [
-    Version(formal="15.0",      short="15",    ip="127.0.0.150", tags={"150"}),
-    Version(formal="16.0",      short="16",    ip="127.0.0.160", tags={"160"}),
-    Version(formal="17.0",      short="17",    ip="127.0.0.170", tags={"170"}),
-    Version(formal="saas-17.2", short="17.2",  ip="127.0.0.172", tags={"172"}),
-    Version(formal="saas-17.4", short="17.4",  ip="127.0.0.174", tags={"174"}),
-    Version(formal="18.0",      short="18",    ip="127.0.0.180", tags={"18"}),
-    Version(formal="saas-18.1", short="18.1",  ip="127.0.0.181", tags={"181"}),
-    Version(formal="saas-18.2", short="18.2",  ip="127.0.0.182", tags={"182"}),
-    Version(formal="master",    short="master",ip="127.0.0.254", tags={"ms"}),
+    Version(formal="15.0",      short="15",    ip="127.0.1.150", tags={"150"}),
+    Version(formal="16.0",      short="16",    ip="127.0.1.160", tags={"160"}),
+    Version(formal="17.0",      short="17",    ip="127.0.1.170", tags={"170"}),
+    Version(formal="saas-17.2", short="17.2",  ip="127.0.1.172", tags={"172"}),
+    Version(formal="saas-17.4", short="17.4",  ip="127.0.1.174", tags={"174"}),
+    Version(formal="18.0",      short="18",    ip="127.0.1.180", tags={"18"}),
+    Version(formal="saas-18.1", short="18.1",  ip="127.0.1.181", tags={"181"}),
+    Version(formal="saas-18.2", short="18.2",  ip="127.0.1.182", tags={"182"}),
+    Version(formal="master",    short="master",ip="127.0.1.254", tags={"ms"}),
 ]
 
 
